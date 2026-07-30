107d1bd970f9835c185355afc1645f6fd6f34ae6
xyperine <ewsannne@gmail.com>
2026-07-30 21:54:17 +0300

    Added ESlint

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import js from "@eslint/js";
+import globals from "globals";
+import markdown from "@eslint/markdown";
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
+  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+]);
