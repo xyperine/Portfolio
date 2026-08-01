22e294e75d108e1053b896d100c05fd649c5a6f3
xyperine <ewsannne@gmail.com>
2026-08-01 23:23:02 +0300

    Added App class.

---
+++ src/app.js
@@ -0,0 +1,83 @@
+import { SimpleWorld } from '#src/simpleWorld.js';
+import { InteractiveWorld } from '#src/interactiveWorld.js';
+
+export class App {
+    constructor() {
+        this.settings = {
+            darkMode: false,
+            interactive: false,
+        }
+
+        this.mainElement = document.querySelector("main");
+        this.darkModeSwitch = document.querySelector("#dark-mode-switch");
+        this.modeSwitch = document.querySelector("#mode-switch");
+
+        this.simpleWorld;
+        this.interactiveWorld;
+
+        this.darkModeSwitch.addEventListener("click", () => {
+            this.settings.darkMode = !this.settings.darkMode;
+            
+            document.documentElement.classList.toggle("dark", this.settings.darkMode);
+            
+            this.darkModeSwitch.textContent = this.settings.darkMode 
+            ? "Dark"
+            : "Light";
+            
+            this.onDarkModeChanged(this.settings.darkMode);
+        })
+
+        this.modeSwitch.addEventListener("click", () => {
+            this.settings.interactive = !this.settings.interactive;
+            
+            this.modeSwitch.textContent = this.settings.interactive
+            ? "Interactive"
+            : "Simple";
+
+            if (this.settings.interactive) {
+                this.simpleWorld = null;
+                this.init();
+            } else {
+                this.interactiveWorld = null;
+                this.init();
+            }
+        })
+
+        this.init();
+    }
+
+    
+    onDarkModeChanged(newValue) {
+        if (this.settings.interactive) {
+            this.interactiveWorld.updateColors();
+        } else {
+            this.simpleWorld.updateColors();
+        }
+    }
+
+
+    init() {
+        if (this.settings.interactive) {
+            this.interactiveWorld = new InteractiveWorld();
+            window.addEventListener("resize", () => {
+                this.resize();
+            });
+            this.interactiveWorld.resize(this.mainElement.clientWidth, this.mainElement.clientHeight);
+        } else {
+            this.simpleWorld = new SimpleWorld();
+            window.addEventListener("resize", () => {
+                this.resize();
+            });
+            this.simpleWorld.resize(this.mainElement.clientWidth, this.mainElement.clientHeight);
+        }
+    }
+
+
+    resize() {
+        if (this.settings.interactive) {
+            this.interactiveWorld.resize(this.mainElement.clientWidth, this.mainElement.clientHeight);
+        } else {
+            this.simpleWorld.resize(this.mainElement.clientWidth, this.mainElement.clientHeight);
+        }
+    }
+}
