487c7442fbd165756c42e78465e60cb1a4bed1b5
xyperine <ewsannne@gmail.com>
2026-08-04 16:17:17 +0300

    Made FPSCounter fields private.

--- src/fpsCounter.js
+++ src/fpsCounter.js
@@ -1,12 +1,15 @@
 export class FPSCounter {
+    #prevTime;
+    #fpsElement;
+
     constructor() {
-        this.prevTime = performance.now();
-        this.fpsElement = document.querySelector("#fps-counter");
+        this.#prevTime = performance.now();
+        this.#fpsElement = document.querySelector("#fps-counter");
     }
 
     update() {
-        const fps = 1000 / (performance.now() - this.prevTime);
-        this.prevTime = performance.now();
-        this.fpsElement.textContent = `${Math.round(fps)} FPS`;
+        const fps = 1000 / (performance.now() - this.#prevTime);
+        this.#prevTime = performance.now();
+        this.#fpsElement.textContent = `${Math.round(fps)} FPS`;
     }
 }
