d81a6f0592e8cf89abaa5ab767872fd648038f56
xyperine <ewsannne@gmail.com>
2026-08-22 16:17:52 +0300

    Applied some linter suggestions.

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -3,6 +3,8 @@ export default {
   extends: ["stylelint-config-standard"],
   rules: {
     "custom-property-empty-line-before" : null,
-    "declaration-empty-line-before" : null
+    "declaration-empty-line-before" : null,
+    "color-function-notation": "legacy",
+    "hue-degree-notation": "number"
   }
 };
