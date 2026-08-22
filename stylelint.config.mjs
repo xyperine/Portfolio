004bef39a336c88facd36461b92c43156c2e811b
xyperine <ewsannne@gmail.com>
2026-08-22 15:05:32 +0300

    Disabled empty line rules for Stylelint.

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import("stylelint").Config} */
 export default {
-  extends: ["stylelint-config-standard"]
+  extends: ["stylelint-config-standard"],
+  rules: {
+    "custom-property-empty-line-before" : null,
+    "declaration-empty-line-before" : null
+  }
 };
