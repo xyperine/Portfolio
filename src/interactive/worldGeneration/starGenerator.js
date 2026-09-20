8f83ba335c5e26b2222157e220a7cdda1dcde8a4
xyperine <ewsannne@gmail.com>
2026-09-20 21:43:28 +0300

    Removed unused code from StarGenerator class.

--- src/interactive/worldGeneration/starGenerator.js
+++ src/interactive/worldGeneration/starGenerator.js
@@ -13,28 +13,16 @@ export class StarGenerator {
 		);
 		const SUN_LUMINOSITY = 3.828e26;
 		const SUN_RADIUS = 6.957e8;
-		//const mass = this.generateMass();
 		const radius =
 			SUN_RADIUS *
 			Math.pow(
 				10,
 				utils.seededGaussianConstrained(this.random, -2, 2, 0, 0.8),
-			); //Math.pow(mass, 0.8);
+			);
 		const luminosity =
 			SUN_LUMINOSITY *
-			Math.pow(10, utils.seededGaussian(this.random, -1, 1)); //Math.pow(mass, 3.5);
+			Math.pow(10, utils.seededGaussian(this.random, -1, 1));
 
 		return new Star(planetsInSystem, luminosity, radius);
-	}
-
-	// shit
-	generateMass() {
-		const min = 0.01;
-		const max = 20;
-
-		const u = this.random();
-
-		const mass = min * Math.pow(max / min, u);
-		return mass;
 	}
 }
